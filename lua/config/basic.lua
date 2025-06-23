c4a3e3d9c78d5e667d289d4cc68447eda7436921
zht7063 <zht7063@163.com>
2025-06-23 20:26:25 +0800

    [Update] 将 init.lua 中的内容分散到了 lua/config 的子文件中（主要是basic.lua和keymap.lua），并且添加了<C-z>作为撤销功能的快捷键。

---
+++ lua/config/basic.lua
@@ -0,0 +1,18 @@
+----- ----- ----- -----
+-- Basic Settings
+----- ----- ----- -----
+
+-- Show line number and make it relative
+vim.opt.number = true
+vim.opt.relativenumber = true
+-- Highlight current line.
+vim.opt.cursorline = true
+-- Highlight specified column.
+vim.opt.colorcolumn = '80'
+-- Set TAB as spaces.
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 0
+-- Auto load extra changes
+vim.opt.autoread = true
+
---
+++ lua/config/keymap.lua
@@ -0,0 +1,4 @@
+vim.keymap.set({"n", "i"}, "<C-a>b", "<Cmd>lua print('SKY IS YOUR LIMIT!')<CR>", {silent=true})
+
+vim.keymap.set({"n", "i" }, "<C-z>", "<Cmd>undo<CR>", {silent=true})
+
