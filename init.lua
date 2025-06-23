e3050bc75528689b9a72a886a601783bd3709da5
tiansang <zht7063@163.com>
2025-06-23 17:23:22 +0800

    Remake[ALL]: Remake config files.

--- init.lua
+++ init.lua
@@ -1,4 +1,26 @@
 -- This is nvim config file.
 
-require("core.basic")
-require("config.lazy")
+----- ----- ----- -----
+-- import files.
+----- ----- ----- -----
+-- require("config.lazy")
+
+----- ----- ----- -----
+-- Basic Settings
+----- ----- ----- -----
+
+-- Show line number and make it relative
+vim.opt.number = true
+vim.opt.relativenumber = true
+-- Highlight current line.
+vim.opt.cursorline = true
+-- Highlight specified column.
+vim.opt.colorcolumn = '80'
+-- Set TAB as spaces.
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 0
+
+-- Auto load extra change
+vim.opt.autoread = true
+
