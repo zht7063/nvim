ab2904959428065624dfff77e8e45077d1e5ccad
zht7063 <zht7063@163.com>
2025-06-23 20:30:04 +0800

    [Fix] 补充提交

--- init.lua
+++ init.lua
@@ -4,25 +4,6 @@
 -- import files.
 ----- ----- ----- -----
 require("config.lazy")
-
-
------ ----- ----- -----
------ ----- ----- -----
--- Basic Settings
------ ----- ----- -----
-
--- Show line number and make it relative
-vim.opt.number = true
-vim.opt.relativenumber = true
--- Highlight current line.
-vim.opt.cursorline = true
--- Highlight specified column.
-vim.opt.colorcolumn = '80'
--- Set TAB as spaces.
-vim.opt.expandtab = true
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 0
-
--- Auto load extra change
-vim.opt.autoread = true
+require("config.basic")
+require("config.keymap")
 
