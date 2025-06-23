5d6ae571c01b2f6e758e71263d3160f8a5270199
zht7063 <zht7063@163.com>
2025-06-23 20:40:36 +0800

    [更新] 在配置文件 config/keymap.lua 中，添加了 leader key 的定义和使用方法，以及内置匿名函数作为 rhs 的用法，现在有三种不同的快捷键映射方式在配置文件中，可以按需选择。

--- lua/config/keymap.lua
+++ lua/config/keymap.lua
@@ -1,4 +1,11 @@
 vim.keymap.set({"n", "i"}, "<C-a>b", "<Cmd>lua print('SKY IS YOUR LIMIT!')<CR>", {silent=true})
 
+vim.keymap.set("n", "<C-a>c", function()
+    print("called lua function.")
+end, {silent = true})
+
 vim.keymap.set({"n", "i" }, "<C-z>", "<Cmd>undo<CR>", {silent=true})
+
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>talk", ":lua print('hello world!')<CR>", {silent=true})
 
